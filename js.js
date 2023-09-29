b6b8c3a4ff9da8352db4327c07c30b87baf9fe56
liczy7564 <127211976+liczy7564@users.noreply.github.com>
2023-09-30 05:00:34 +0800

    Add files via upload

--- js.js
+++ js.js
@@ -1,6 +1,26 @@
 $(document).ready(function(){
+  let history_text_0=document.querySelector(".history_text_0");
+  let history_img_0=document.querySelector(".history_img_0");
+  let history_text_1=document.querySelector(".history_text_1");
+  let history_img_1=document.querySelector(".history_img_1");
+  let history_text_2=document.querySelector(".history_text_2");
+  let history_img_2=document.querySelector(".history_img_2");
+  $.ajax({
+    url:"json/自傳.json",
+    success:function(result){
+        history_text_0.innerHTML+=result[0]['內容'];
+        history_img_0.innerHTML+="<img class='about_img' src="+
+        result[0]['圖片']+">";
+        history_text_1.innerHTML+=result[1]['內容'];
+        history_img_1.innerHTML+="<img class='about_img' src="+
+        result[1]['圖片']+">";
+        history_text_2.innerHTML+=result[2]['內容'];
+        history_img_2.innerHTML+="<img class='about_img' src="+
+        result[2]['圖片']+">";
+    }  
+  });
 	let Secondary = document.getElementsByClassName("Secondary"); 
-
+  Secondary[0].style.display = "block";
 	let Skill_1=document.querySelector(".Skill_1");
   $.ajax({
     url:"json/skill_程式.json",
@@ -11,7 +31,18 @@ $(document).ready(function(){
       result[i]['what']+"</div><div class='col'>"+
       result[i]['about']+"</div></div>"
       }
-      Skill_1_content_recall();
+    }
+  });
+  let The_class=document.querySelector(".The_class");
+  $.ajax({
+    url:"json/教學.json",
+    success:function(result){
+      for(let i=0;i<result.length;i++){
+      The_class.innerHTML+="<div class='carousel-item'><div class='row'><div class='col-md-6 col-xs-12'><div class='class_lt'><div class='Activity_title_1'>"+
+      result[i]['名稱']+"</div><div class='class_information'>"+
+      result[i]['資訊']+"<br>"+result[i]['老師']+"<br>"+result[i]['分數']+"</div><div class='class_text'>"+result[i]['心得']+ "</div></div></div><div class='col-md-6 col-xs-12'><div class='Activity_rt'><img class='class_img' src="+
+      result[i]['圖片']+"></img></div></div></div></div>"
+      }
     }
   });
   let Skill_2=document.querySelector(".Skill_2");
@@ -24,7 +55,6 @@ $(document).ready(function(){
       result[i]['what']+"</div><div class='col'>"+
       result[i]['about']+"</div></div>"
       }
-      Skill_2_content_recall();
     }
   });
   let Skill_3=document.querySelector(".Skill_3");
@@ -37,7 +67,6 @@ $(document).ready(function(){
       result[i]['what']+"</div><div class='col'>"+
       result[i]['about']+"</div></div>"
       }
-      Skill_3_content_recall();
     }
   });
  
@@ -59,10 +88,21 @@ $(document).ready(function(){
       result[i]['Certifications']+"><img class='SCert_img' src="+
       result[i]['SCert_img']+"></div>"
       }
-      container_Certifications_content_recall();
     }
   });
-
+//主控制開啟副控制
+// background-color: #000;
+// border-radius: 40px;
+$(".illustrate").click(function(){
+  $(".The_illustrate").slideToggle();
+}); 
+$(".Table").click(function(){
+  $(".The_Table").slideToggle();
+}); 
+$(".a1").on("click",function(){
+  Secondary[2].style.display = "block";
+});
+let Main_option= document.getElementsByClassName("Main_options"); 
 	$(".Main_options").on("click",function(){
     let num = $(this).attr("value");
     Main_options(num);
@@ -70,13 +110,17 @@ $(document).ready(function(){
   function Main_options(num) { 
     let i;
     for (i = 0; i < Secondary.length; i++) {
-      Secondary[i].style.display = "none"; 
+      Secondary[i].style.display = "none";
+      Main_option[i].style.color="#B0B0B0"; 
     }
-    Secondary[num].style.display = "block"; 
+    Secondary[num].style.display = "block";
+    Main_option[num].style.color="#474032";
   }
+  // 置頂
 $(function(){
   $(".top-Anchor").click(function(){
     $("html,body").animate({scrollTop:0},10);
   });
 })
 });
+
